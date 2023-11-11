0b63739e1c593116c60e025e89dbee7ae7da664a
Aleksandr Tekutev <tekutiev.sasha@gmail.com>
2023-11-11 15:36:33 +0000

    added tests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,4 @@
 import os
-
 from pathlib import Path
 
 
@@ -10,7 +9,6 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
